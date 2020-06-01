0ed4da984dbf3e524ecf3452eaa2a727a0b9d985
Darshit <darshitparekh30@gmail.com>
2020-06-01 19:30:44 +0530

    Two test cases handled

--- src/main/java/com/cal/Calculator.java
+++ src/main/java/com/cal/Calculator.java
@@ -3,8 +3,16 @@ package com.cal;
 public class Calculator {
 
 	public void add(String string) {
-		// TODO Auto-generated method stub
-		
+				String array[] = string.split(",");
+				if(array.length>2) {
+					throw new RuntimeException("Only two numbers are allowed");
+				}
+				else {
+					for(String x:array) {
+						Integer.parseInt(x);
+					}
+				}
+	
 	}
 
 }
